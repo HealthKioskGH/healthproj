69e4417cdf69221a23190939e1be1bd8d8a7198f
moibrahh <ibrahimmohammed.im900@gmail.com>
2025-02-19 11:31:22 +0000

    Add missing fields migration

---
+++ accounts/migrations/0002_add_missing_fields.py
@@ -0,0 +1,61 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='User',
+            name='address',
+            field=models.TextField(blank=True),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='phone_number',
+            field=models.CharField(blank=True, max_length=15),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='qualifications',
+            field=models.TextField(blank=True),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='hospital_affiliations',
+            field=models.TextField(blank=True),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='languages',
+            field=models.CharField(blank=True, max_length=200),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='working_hours',
+            field=models.CharField(blank=True, max_length=100),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='emergency_contact',
+            field=models.CharField(blank=True, max_length=200),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='office_address',
+            field=models.TextField(blank=True),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='assigned_doctor',
+            field=models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, related_name='assigned_patients', limit_choices_to={'user_type': 'doctor'}),
+        ),
+        migrations.AddField(
+            model_name='User',
+            name='profile_picture',
+            field=models.ImageField(blank=True, null=True, upload_to='profile_pics/'),
+        ),
+    ] 
